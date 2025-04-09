13993b29356408e1badbdccdd93c059150c4aa92
Jomar Santander <jesantander7@soy.sena.edu.co>
2025-04-09 18:38:34 -0500

    conexion log admin

--- src/main/java/com/moi/controller/administratorOption/AdministratorOptionServletController.java
+++ src/main/java/com/moi/controller/administratorOption/AdministratorOptionServletController.java
@@ -14,4 +14,10 @@ public class AdministratorOptionServletController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        request.getRequestDispatcher("/WEB-INF/views/administratorOptions/administrationOption.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+
+    }
 }
